26c5e7911cb271c81d5af27ead51b0da1a12f007
AnushGurung11 <anushgrg15@gmai.com>
2025-06-25 11:54:40 +0545

    Frame Created

--- src/Interface.java
+++ src/Interface.java
@@ -1,7 +1,27 @@
 
 /*
-Now
+Using swing and awt for the user interface
+
  */
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Interface {
+    public static void main(String[] args){
+        //TODO creating Frame for the landing page
+        //TODO creating buttons for deposit, withdraw, Create new account
+
+        JFrame frame = new JFrame("ATM-Simulator");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(3,1));
+        frame.setSize(1080, 720);
+
+
+
+
+
+
+    }
 }
